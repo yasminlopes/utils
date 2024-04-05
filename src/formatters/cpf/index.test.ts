2c4a5466b47e45a22fca8c901731154ec581c7ef
Yasmin Lopes <yclopes27@gmail.com>
2024-04-05 11:31:24 -0300

    test: ensure formatCpf format number cpf with mask

--- src/formatters/cpf/index.test.ts
+++ src/formatters/cpf/index.test.ts
@@ -16,4 +16,18 @@ describe('formatCpf', () => {
     expect(formatCpf('94389575104')).toBe('943.895.751-04');
   });
 
+  it('should format number CPF with mask', () => {
+    expect(formatCpf(9)).toBe('9');
+    expect(formatCpf(94)).toBe('94');
+    expect(formatCpf(943)).toBe('943');
+    expect(formatCpf(9438)).toBe('943.8');
+    expect(formatCpf(94389)).toBe('943.89');
+    expect(formatCpf(943895)).toBe('943.895');
+    expect(formatCpf(9438957)).toBe('943.895.7');
+    expect(formatCpf(94389575)).toBe('943.895.75');
+    expect(formatCpf(943895751)).toBe('943.895.751');
+    expect(formatCpf(9438957510)).toBe('943.895.751-0');
+    expect(formatCpf(94389575104)).toBe('943.895.751-04');
+  });
+
 });
