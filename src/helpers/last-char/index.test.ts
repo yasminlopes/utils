7909ff2014d5696609f7385b243033471cc7b170
Yasmin Lopes <yclopes27@gmail.com>
2024-04-05 11:25:48 -0300

    test: ensure it returns false when index is NOT the same as last index of the string

--- src/helpers/last-char/index.test.ts
+++ src/helpers/last-char/index.test.ts
@@ -5,4 +5,8 @@ describe('isLastChar', () => {
     expect(isLastChar(2, '123')).toBe(true);
   });
 
+  it('should return false when index is NOT the same as last index of the string', () => {
+    expect(isLastChar(1, '123')).toBe(false);
+    expect(isLastChar(3, '123')).toBe(false);
+  });
 });
