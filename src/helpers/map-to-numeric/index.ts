3b55aa37b074b99686457c5c2a333e64584c6f29
Yasmin Lopes <yclopes27@gmail.com>
2024-04-05 11:14:59 -0300

    docs: add mapToNumeric explanation and example of use cases

--- src/helpers/map-to-numeric/index.ts
+++ src/helpers/map-to-numeric/index.ts
@@ -1,2 +1,13 @@
 import { RegExHelper } from 'helpers/regex';
+/**
+ * Remove todos os caracteres não numéricos de uma string.
+ *
+ * @param value A string a ser mapeada para caracteres numéricos.
+ * @returns A string resultante contendo apenas caracteres numéricos.
+ */
 export const mapToNumeric = (value: string) => value.replace(RegExHelper.nonDigit, '');
+
+// Exemplos de uso:
+// console.log(mapToNumeric('abc123def456')); // '123456'
+// console.log(mapToNumeric('R$ 1.234,56')); // '123456'
+// console.log(mapToNumeric('12-34-5678')); // '12345678'
