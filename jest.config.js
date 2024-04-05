e193c217081be4b416fc1b65b6189cee60a4ae46
Yasmin Lopes <yclopes27@gmail.com>
2024-04-05 11:54:31 -0300

    refactor: cleanup module name mapping for paths

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,9 @@ module.exports = {
     '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    '^helpers$': '<rootDir>/src/helpers', // Serve para importar arquivos sem precisar de caminho relativo
-    '^helpers/(.*)$': '<rootDir>/src/helpers/$1',
-    '^validators$': '<rootDir>/src/validators', // Serve para importar arquivos sem precisar de caminho relativo
-    '^validators/(.*)$': '<rootDir>/src/validators/$1',
-    '^common/(.*)$': '<rootDir>/src/common/$1',
-    '^common/(.*)$': '<rootDir>/src/common/$1',
+    '^helpers(/.*)?$': '<rootDir>/src/helpers$1',
+    '^validators(/.*)?$': '<rootDir>/src/validators$1',
+    '^common(/.*)?$': '<rootDir>/src/common$1',
+    '^formatters(/.*)?$': '<rootDir>/src/formatters$1',
   },
 };
