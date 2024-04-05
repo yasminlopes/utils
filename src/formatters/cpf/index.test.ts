09c734f43da81fb4e5b4b115c8a201e9595d5187
Yasmin Lopes <yclopes27@gmail.com>
2024-04-05 11:31:39 -0300

    test: ensure formatCpf format cpf with mask filling zeroes

--- src/formatters/cpf/index.test.ts
+++ src/formatters/cpf/index.test.ts
@@ -30,4 +30,19 @@ describe('formatCpf', () => {
     expect(formatCpf(94389575104)).toBe('943.895.751-04');
   });
 
+  it('should format CPF with mask filling zeroes', () => {
+    expect(formatCpf('', { pad: true })).toBe('000.000.000-00');
+    expect(formatCpf('9', { pad: true })).toBe('000.000.000-09');
+    expect(formatCpf('94', { pad: true })).toBe('000.000.000-94');
+    expect(formatCpf('943', { pad: true })).toBe('000.000.009-43');
+    expect(formatCpf('9438', { pad: true })).toBe('000.000.094-38');
+    expect(formatCpf('94389', { pad: true })).toBe('000.000.943-89');
+    expect(formatCpf('943895', { pad: true })).toBe('000.009.438-95');
+    expect(formatCpf('9438957', { pad: true })).toBe('000.094.389-57');
+    expect(formatCpf('94389575', { pad: true })).toBe('000.943.895-75');
+    expect(formatCpf('943895751', { pad: true })).toBe('009.438.957-51');
+    expect(formatCpf('9438957510', { pad: true })).toBe('094.389.575-10');
+    expect(formatCpf('94389575104', { pad: true })).toBe('943.895.751-04');
+  });
+
 });
